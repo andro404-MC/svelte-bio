6e43dc695b1b596a238a73c7a58a70b172137842
Untemi <94703538+andro404-MC@users.noreply.github.com>
2023-11-28 18:25:27 +0100

    wow its been a while since i used git

---
+++ src/lib/config.js
@@ -0,0 +1,31 @@
+export let profileLink = "https://avatars.githubusercontent.com/u/94703538?v=4"
+export let name = "untemi"
+export let kofiLink = "https://ko-fi.com/untemi"
+
+export let items = [
+  {
+    title : "Facebook",
+    paragraph : "abdelfettah jalllali",
+    link : "https://facebook.com",
+    imageLink : "https://untemi.sirv.com/bio/facebook.webp",
+    imageColor : "white",
+  },{
+    title : "Instagram",
+    paragraph : "@untemi",
+    link : "https://instagram.com",
+    imageLink : "https://untemi.sirv.com/bio/instagram.webp",
+    imageColor : "white",
+  },{
+    title : "YouTube",
+    paragraph : "@untemi0",
+    link : "https://youtube.com",
+    imageLink : "https://untemi.sirv.com/bio/youtube.webp",
+    imageColor : "white",
+  },{
+    title : "WhatsApp",
+    paragraph : "+212 61 23 45 67 89",
+    link : "https://whatsapp.com",
+    imageLink : "https://untemi.sirv.com/bio/whatsapp.webp",
+    imageColor : "#fcf5eb",
+  },
+]
