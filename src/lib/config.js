d88eb6869e83d04de538d873a7605f39d6fc0c20
Untemi <94703538+andro404-MC@users.noreply.github.com>
2023-12-01 09:25:15 +0100

    for fiverr

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,24 +1,24 @@
 export let profileLink = "https://avatars.githubusercontent.com/u/94703538?v=4"
-export let name = "Untemi"
+export let name = "Name"
 export let favicon = "https://upload.wikimedia.org/wikipedia/commons/6/6e/Svelte_logo_by_gengns.svg"
-export let kofiLink = "https://ko-fi.com/untemi"
+export let kofiLink = "https://ko-fi.com/"
 /* image 545x180 */
 export let items = [
   {
     title : "Facebook",
-    paragraph : "abdelfettah jalllali",
+    paragraph : "Page name",
     link : "https://facebook.com",
     imageLink : "https://untemi.sirv.com/bio/facebook.webp",
     imageColor : "white",
   },{
     title : "Instagram",
-    paragraph : "@untemi",
-    link : "https://instagram.com",
+    paragraph : "account name",
+    link : "https://instagram.cominstagram.com",
     imageLink : "https://untemi.sirv.com/bio/instagram.webp",
     imageColor : "white",
   },{
     title : "YouTube",
-    paragraph : "@untemi0",
+    paragraph : "account name",
     link : "https://youtube.com",
     imageLink : "https://untemi.sirv.com/bio/youtube.webp",
     imageColor : "white",
@@ -28,5 +28,53 @@ export let items = [
     link : "https://whatsapp.com",
     imageLink : "https://untemi.sirv.com/bio/whatsapp.webp",
     imageColor : "white",
-  },
+  },{
+    title : "Discord Server",
+    paragraph : "server name",
+    link : "https://discord.com",
+    imageLink : "https://untemi.sirv.com/bio/discord-s.webp",
+    imageColor : "white",
+  },{
+    title : "X",
+    paragraph : "account name",
+    link : "https://x.com",
+    imageLink : "https://untemi.sirv.com/bio/x.webp",
+    imageColor : "white",
+  },{
+    title : "Pinterest",
+    paragraph : "account name",
+    link : "https://x.com",
+    imageLink : "https://untemi.sirv.com/bio/pinterest.webp",
+    imageColor : "white",
+  },{
+    title : "Reddit",
+    paragraph : "account name",
+    link : "https://reddit.com",
+    imageLink : "https://untemi.sirv.com/bio/reddit-l.webp",
+    imageColor : "white",
+  },{
+    title : "LinkedIn",
+    paragraph : "account name",
+    link : "https://LinkedIn.com",
+    imageLink : "https://untemi.sirv.com/bio/LinkedIn.webp",
+    imageColor : "white",
+  },{
+    title : "Twitch",
+    paragraph : "account name",
+    link : "https://Twitch.com",
+    imageLink : "https://untemi.sirv.com/bio/twitch.webp",
+    imageColor : "white",
+  },{
+    title : "TikTok",
+    paragraph : "account name",
+    link : "https://TikTok.com",
+    imageLink : "https://untemi.sirv.com/bio/tiktok.webp",
+    imageColor : "white",
+  },{
+    title : "Snapchat",
+    paragraph : "account name",
+    link : "https://TikTok.com",
+    imageLink : "https://untemi.sirv.com/bio/snapchat.webp",
+    imageColor : "white",
+  }
 ]
